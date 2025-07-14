4c3e59211b4df955a61149b046092cbbe650220e
hazem sghaier <h.sghaier@proxiad.com>
2025-07-14 16:59:32 +0200

    Clear main branch

--- src/main/resources/db/migration/sql/student.sql
+++
@@ -1,8 +0,0 @@
-CREATE table student (
-    id serial primary key,
-    first_name varchar(255),
-    last_name varchar(255),
-    email varchar(255),
-    phone varchar(255),
-    age int
-);
--- src/main/resources/db/migration/sql/update-student.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE student ADD COLUMN password VARCHAR(255);
