a9232d9fbf09dad8087e5cec0afd7e634d449330
hazem sghaier <h.sghaier@proxiad.com>
2025-07-15 17:25:37 +0200

    fix : fix liquibase configurations and the sqlfor user creation

--- database-integration/src/main/resources/db/changelog/change-Sets/001-create-users-table.sql
+++ database-integration/src/main/resources/db/changelog/change-Sets/001-create-users-table.sql
@@ -1,9 +1,9 @@
 create table users(
-    id serial primary key,
-    name varchar(255),
-    email varchar(255),
-    password varchar(255)
-    address varchar(255),
-    phone varchar(255),
-    age integer
+                      id serial primary key,
+                      name varchar(255),
+                      email varchar(255),
+                      password varchar(255),
+                      address varchar(255),
+                      phone varchar(255),
+                      age integer
 );
