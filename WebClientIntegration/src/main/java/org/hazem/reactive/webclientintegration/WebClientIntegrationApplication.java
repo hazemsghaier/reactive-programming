09e2c76494dc4890767e956eade00ac0ff3d68ae
hazem sghaier <h.sghaier@proxiad.com>
2025-07-18 13:58:59 +0200

    feat (project scheama): crée le sheam du projet initial

---
+++ WebClientIntegration/src/main/java/org/hazem/reactive/webclientintegration/WebClientIntegrationApplication.java
@@ -0,0 +1,13 @@
+package org.hazem.reactive.webclientintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebClientIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebClientIntegrationApplication.class, args);
+    }
+
+}
---
+++ WebClientIntegration/src/test/java/org/hazem/reactive/webclientintegration/WebClientIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hazem.reactive.webclientintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebClientIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
